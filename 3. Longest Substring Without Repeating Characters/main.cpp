303a232ec33b69707196bb66d8abd04b18c94e28
Jim Moua <jimmoua24@gmail.com>
2021-06-27 13:03:18 -0500

    Better runtime and space complexity

--- 3. Longest Substring Without Repeating Characters/main.cpp
+++ 3. Longest Substring Without Repeating Characters/main.cpp
@@ -6,27 +6,19 @@ using namespace std;
 class Solution {
 public:
   int lengthOfLongestSubstring(string s) {
-    if(s.length() == 1 || s.length() == 0) return s.length();
-
-    // Sliding window approach
-    unordered_map<char, int> mp;
-    size_t l=0;
-    string window, longest("");
-    for(int i = 0; i < s.length(); i++) {
-      window.push_back(s[i]);
-      mp[s[i]]++;
-      if(mp[s[i]] > 1) {
-        l = s.find_last_of(s[i], i-1)+1;
-        size_t p = window.find(s[i]);
-        string ll = window.substr( p+1 );
-        window.clear(); mp.clear();
-        window = ll;
-        for(const auto& d : window) mp[d]++;
-      } else {
-        if(window.length() > longest.length()) longest = window;
+    array<char, 256> arr{0};
+    int rp=0, lp=0;
+    int ans=0;
+    while(rp < s.length()) {
+      arr[s[rp]]++;
+      while(arr[s[rp]] > 1) {
+        arr[s[lp]]--;
+        lp++;
       }
+      ans = max(ans, rp-lp+1);
+      rp++;
     }
-    return (int)longest.length();
+    return ans;
   };
 };
 
