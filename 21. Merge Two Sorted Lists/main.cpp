5be36b889bf06712e23366a4ccb1434183d15893
Jim Moua <jimmoua24@gmail.com>
2021-07-05 09:21:13 -0500

    LC 21

---
+++ 21. Merge Two Sorted Lists/main.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+#include <gtest/gtest.h>
+
+using namespace std;
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+  ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+    if(l1 == nullptr) return l2;
+    if(l2 == nullptr) return l1;
+    if(l1->val <= l2->val) {
+      l1->next = mergeTwoLists(l1->next, l2);
+      return l1;
+    } else {
+      l2->next = mergeTwoLists(l2->next, l1);
+      return l2;
+    }
+  }
+};
+
+TEST(TestSuite, TestCase) {
+  EXPECT_EQ(1+1, 2);
+}
+
+int main(int argc, char* argv[]) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
